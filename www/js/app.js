51bbe558253dff5451718caaa6b6c6d69c056d8c
Liner236 <marc.piontek@web.de>
2016-09-29 14:45:13 +0200

    jojojo+

---
+++ www/js/app.js
@@ -0,0 +1,105 @@
+// Ionic Starter App
+
+// angular.module is a global place for creating, registering and retrieving Angular modules
+// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
+// the 2nd parameter is an array of 'requires'
+// 'starter.services' is found in services.js
+// 'starter.controllers' is found in controllers.js
+angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
+
+.run(function($ionicPlatform) {
+  $ionicPlatform.ready(function() {
+    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+    // for form inputs)
+    if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
+      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+      cordova.plugins.Keyboard.disableScroll(true);
+
+    }
+    if (window.StatusBar) {
+      // org.apache.cordova.statusbar required
+      StatusBar.styleDefault();
+    }
+  });
+})
+
+.config(function($stateProvider, $urlRouterProvider) {
+
+  // Ionic uses AngularUI Router which uses the concept of states
+  // Learn more here: https://github.com/angular-ui/ui-router
+  // Set up the various states which the app can be in.
+  // Each state's controller can be found in controllers.js
+  $stateProvider
+    .state('login', {
+      url:'/',
+      templateUrl: 'templates/login.html'
+    })
+
+    .state('tabs', {
+        url:'/tabs',
+        abstract: true,
+        templateUrl: 'templates/side_menu.html'
+      })
+
+    .state('tabs.home', {
+      url:'/home',
+      views: {
+        'menuContent':{
+          templateUrl: 'templates/home.html'
+        }
+      }
+    })
+
+    .state('tabs.meine_crews', {
+      url:'/meine_crews',
+      views: {
+        'menuContent':{
+          templateUrl: 'templates/meine_crews.html'
+        }
+      }
+    })
+
+    .state('tabs.crew_erstellen', {
+      url:'/crew_erstellen',
+      views: {
+        'menuContent':{
+          templateUrl: 'templates/crew_erstellen.html'
+        }
+      }
+    })
+
+    .state('tabs.crew_suche', {
+      url:'/crew_suche',
+      views: {
+        'menuContent':{
+          templateUrl: 'templates/crew_suche.html'
+        }
+      }
+    })
+
+    .state('tabs.login', {
+      url:'/login',
+      views: {
+        'menuContent':{
+          templateUrl: 'templates/login.html'
+        }
+      }
+    })
+
+    .state('tabs.break', {
+      url:'/break',
+      views: {
+        'menuContent':{
+          templateUrl: 'templates/break.html'
+        }
+      }
+    })
+
+
+
+  ;
+
+  // if none of the above states are matched, use this as the fallback
+  $urlRouterProvider.otherwise('/tabs/home');
+
+});
---
+++ www/js/controllers.js
@@ -0,0 +1,79 @@
+angular.module('starter.controllers', [])
+
+.controller('DashCtrl', function($scope) {})
+
+.controller('ChatsCtrl', function($scope, Chats) {
+  // With the new view caching in Ionic, Controllers are only called
+  // when they are recreated or on app start, instead of every page change.
+  // To listen for when this page is active (for example, to refresh data),
+  // listen for the $ionicView.enter event:
+  //
+  //$scope.$on('$ionicView.enter', function(e) {
+  //});
+
+  $scope.chats = Chats.all();
+  $scope.remove = function(chat) {
+    Chats.remove(chat);
+  };
+})
+
+
+.controller('ChatDetailCtrl', function($scope, $stateParams, Chats) {
+  $scope.chat = Chats.get($stateParams.chatId);
+})
+
+.controller('AccountCtrl', function($scope) {
+  $scope.settings = {
+    enableFriends: true
+  };
+});
+
+// light saber gyro
+module.controller('ThisCtrl', function($cordovaDeviceMotion) {
+
+  document.addEventListener("deviceready", function () {
+
+    $cordovaDeviceMotion.getCurrentAcceleration().then(function(result) {
+      var X = result.x;
+      var Y = result.y;
+      var Z = result.z;
+      var timeStamp = result.timestamp;
+    }, function(err) {
+      // An error occurred. Show a message to the user
+    });
+
+  }, false);
+
+
+  // watch Acceleration
+  var options = { frequency: 20000 };
+
+  document.addEventListener("deviceready", function () {
+
+    var watch = $cordovaDeviceMotion.watchAcceleration(options);
+    watch.then(
+      null,
+      function(error) {
+        // An error occurred
+      },
+      function(result) {
+        var X = result.x;
+        var Y = result.y;
+        var Z = result.z;
+        var timeStamp = result.timestamp;
+      });
+
+
+    watch.clearWatch();
+    // OR
+    $cordovaDeviceMotion.clearWatch(watch)
+      .then(function(result) {
+        // success
+      }, function (error) {
+        // error
+      });
+
+  }, false);
+});
+
+
